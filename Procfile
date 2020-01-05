62ccbbe64c38ac30fd922d4ab404478c7fca3b1a
HilelBenhamou <hilelbenhamou@hotmail.fr>
2020-01-05 12:38:46 +0200

    name

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn dajngo_project.wsgi --log-file -
---
+++ data.py
@@ -0,0 +1,9 @@
+# import requests
+# import json
+# from pprint import pprint
+#
+#
+# response = requests.get('https://swapi.co/api/people/1/')
+# data = response.json()
+#
+# pprint(data)
